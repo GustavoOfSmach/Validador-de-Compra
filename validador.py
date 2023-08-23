0799b557a2835604e980c8ffbd1bdb9d132e7515
Gustavo Vieira <76416937+GustavoOfSmach@users.noreply.github.com>
2023-08-23 16:09:45 -0300

    Update validador.py

--- validador.py
+++ validador.py
@@ -2,16 +2,15 @@ __version__ = "0.0.1"
 __author__ = "GustavoOfSmach"
 __license__ = "Unlicense"
 
-print('Sua calculadora já está pronta! Comece a usar!')
-salario = 1000
-idade = 18
-
-input('Me informe seu salario: ')
-input('Me informe sua idade: ')
+print('Já estamos quase finalizando sua compra! Complete os dados abaixo:')
+salario = float(input('Informe seu salario:'))
+idade = int(input('Informe sua idade: '))
 
 if salario >= 1000 and idade >= 18:
 
   print('Compra aprovada')
+  print('Parabens sua compra foi realizada com sucesso')
 
 else:
   print('Compra negada')
+  print('Pedimos desculpas mais não foi possivel aprovar sua compra')
