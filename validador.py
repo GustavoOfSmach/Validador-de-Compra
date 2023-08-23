0ac8df1d1665840526cb4bfd5d0148a268e96c79
Gustavo Vieira <76416937+GustavoOfSmach@users.noreply.github.com>
2023-08-23 09:02:11 -0300

    Create validador.py

---
+++ validador.py
@@ -0,0 +1,13 @@
+print('Sua calculadora já está pronta! Comece a usar!')
+salario = 1000
+idade = 18
+
+input('Me informe seu salario: ')
+input('Me informe sua idade: ')
+
+if salario >= 1000 and idade >= 18:
+
+  print('Compra aprovada')
+
+else:
+  print('Compra negada')
